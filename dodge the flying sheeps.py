a9b6d26ee5bf67384e9fbec90cb708751257839a
beepboopblap <89974500+beepboopblap@users.noreply.github.com>
2022-02-20 16:48:01 +0800

    Update dodge the flying sheeps.py

--- dodge the flying sheeps.py
+++ dodge the flying sheeps.py
@@ -1,15 +1,20 @@
-from curses.ascii import ESC
 import pygame, random 
 from pygame.locals import * 
 from pygame import mixer
 
+
+SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 690
+
 pygame.init()
 fps = pygame.time.Clock()
-window = pygame.display.set_mode((690,900))
+window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 #declare colours, images, sounds, fonts
-x = 240
-y = 680
+x = 690
+y = 900
+player_x = 240
+player_y = 680
 blue = (0,0,255)
 green = (0,255,0)
 red = (255,0,0)
@@ -21,24 +26,25 @@ Calibri40 = pygame.font.SysFont("Calibri", 40)
 player_sprite = pygame.image.load("player_sprite.png").convert_alpha()
 sheep_sprite = pygame.image.load("sheep_sprite.png").convert_alpha()
 sheep_speed = -5
-sheep_y = -50
-sheep_x = random.randrange(0,x)
-
+sheep_x = random.randrange(0,player_x)
+sheep_y = -10
 
 #transformations
 
 player_sprite = pygame.transform.scale(player_sprite, (140,140))
+sheep_sprite = pygame.transform.scale(sheep_sprite, (140,140))
+sheep_sprite1 = pygame.transform.scale(sheep_sprite, (140,140))
+sheep_sprite2 = pygame.transform.scale(sheep_sprite, (140,140))
+sheep_sprite3 = pygame.transform.scale(sheep_sprite, (140,140))
+sheep_sprite4 = pygame.transform.scale(sheep_sprite, (140,140))
 
 #variables for keeping track of my game players etc.
 
 quit = False
 
-x = 240
-y = 680
-space = False
-space_counter = 0
-movex = 0
-movey = 0
+player_x = 240
+player_y = 680
+
 
 #main game loop
 while  not quit:
@@ -50,13 +56,13 @@ while  not quit:
             quit = True
         elif event.type == KEYDOWN:
             if event.key == ord("w"):
-                y = y - 40
+                player_y = player_y - 40
             if event.key == ord("s"):
-                y = y + 40
+                player_y = player_y + 40
             if event.key == ord("a"):
-                x = x - 40
+                player_x = player_x - 40
             if event.key == ord("d"):
-                x = x + 40
+                player_x = player_x + 40
         elif event.type == KEYUP:
             if event.key == K_ESCAPE:
                 pygame.quit()
@@ -64,28 +70,50 @@ while  not quit:
     
 
     #perform calculations
-   
+    if player_x >= 599:
+        player_x = 599
+    if player_x <= -35:
+        player_x = -35
+    if player_y >= 760:
+        player_y = 760
+    if player_y <= 0:
+        player_y = 0
 
     #draw graphics
     window.fill(yellow)
     
-    window.blit(player_sprite, (x,y))
-    pygame.draw.rect(window, black, (5,5 ,680, 890), 4)
+    window.blit(player_sprite, (player_x,player_y))
+    pygame.draw.rect(window, black, (3,3 ,683, 890), 4)
     watch_out_label = Calibri40.render("Watch out for flying sheeps!", 1, red)
     controls = Calibri40.render("Use WASD to move", 1, red)
     window.blit(controls, (150, 300))
     window.blit(watch_out_label, (70, 200))
-    sheep_y = sheep_y + sheep_speed
-    if sheep_y > y:
+   
+    if sheep_y < y:
         
         window.blit(sheep_sprite, (sheep_x, sheep_y))
-        sheep_x = random.randrange(0,x)
-        sheep_y = sheep_y -25
+        sheep_y = sheep_y + 10
+        if sheep_y > 830:
+            sheep_x = random.randrange(0,x)
+            sheep_y = 0
+    
+    if sheep_y < y:
         
+        window.blit(sheep_sprite1, (sheep_x, sheep_y))
+        sheep_y = sheep_y + 10
+        if sheep_y > 830:
+            sheep_x = random.randrange(0,x)
+            sheep_y = 0
+    if sheep_y < y:
+        
+        window.blit(sheep_sprite2, (sheep_x, sheep_y))
+        sheep_y = sheep_y + 10
+        if sheep_y > 830:
+            sheep_x = random.randrange(0,x)
+            sheep_y = 0
     #update
     pygame.display.update()
-    fps.tick(25)
-
+    fps.tick(10)
 #loop over, game over
 pygame.quit()
 
