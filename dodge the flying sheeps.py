70e0ca5674940a1cb0c979dd7d4a15c2bed64519
beepboopblap <89974500+beepboopblap@users.noreply.github.com>
2022-02-20 11:43:16 +0800

    Update dodge the flying sheeps.py

--- dodge the flying sheeps.py
+++ dodge the flying sheeps.py
@@ -17,11 +17,13 @@ yellow = (255,255,0)
 black = (0,0,0)
 white = (255,255,255)
 Calibri60 = pygame.font.SysFont("Calibri", 60)
+Calibri40 = pygame.font.SysFont("Calibri", 40)
 player_sprite = pygame.image.load("player_sprite.png").convert_alpha()
 sheep_sprite = pygame.image.load("sheep_sprite.png").convert_alpha()
 sheep_speed = -5
 sheep_y = -50
 sheep_x = random.randrange(0,x)
+
 
 #transformations
 
@@ -68,12 +70,18 @@ while  not quit:
     window.fill(yellow)
     
     window.blit(player_sprite, (x,y))
+    pygame.draw.rect(window, black, (5,5 ,680, 890), 4)
+    watch_out_label = Calibri40.render("Watch out for flying sheeps!", 1, red)
+    controls = Calibri40.render("Use WASD to move", 1, red)
+    window.blit(controls, (150, 300))
+    window.blit(watch_out_label, (70, 200))
     sheep_y = sheep_y + sheep_speed
     if sheep_y > y:
         
-        sheep_x = random.randrange(0,x)
-        sheep_y = -25
         window.blit(sheep_sprite, (sheep_x, sheep_y))
+        sheep_x = random.randrange(0,x)
+        sheep_y = sheep_y -25
+        
     #update
     pygame.display.update()
     fps.tick(25)
